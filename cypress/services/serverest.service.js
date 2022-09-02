ae0053d0dd60de3228fc8f44d988a4e60c01f80b
dhermys <96082998+dhermys@users.noreply.github.com>
2022-09-02 16:24:57 -0300

    Dias 5 e 6 - Implementação do design Service Object

---
+++ cypress/services/serverest.service.js
@@ -0,0 +1,57 @@
+
+const URL_USUARIOS = '/usuarios'
+const URL_LOGIN = '/login'
+const URL_PRODUTOS = '/produtos'
+const URL_CARRINHOS = '/carrinhos'
+
+export default class Serverest {
+
+    //Ações que podemos realizar na API
+    // Buscar usuários
+    // Cadastrar novos usuários
+    // Realizar login
+
+    static buscarUsuarios() {
+        return cy.rest('GET', URL_USUARIOS)
+    }
+
+    static buscarUsuarioParaLogin() {
+        cy.request(URL_USUARIOS).then(res => {
+            cy.wrap({
+                email: res.body.usuarios[0].email,
+                password: res.body.usuarios[0].password
+            }).as('usuarioLogin')
+        })
+    }
+
+    static logar(usuario) {
+        return cy.rest('POST', URL_LOGIN, usuario)
+    }
+
+    static salvarBearer(resposta) {
+        Cypress.env('bearer', resposta.body.authorization.slice(7))
+    }
+
+    // Produtos //
+
+    static buscarProdutos() {
+        return cy.rest('GET', URL_PRODUTOS)
+    }
+
+    static cadastrarProdutoComSucesso() {
+        return cy.request({
+            method: 'POST',
+            url: URL_PRODUTOS,
+            body: {
+                "nome": "Logico MT Horizontal",
+                "preco": 350,
+                "descricao": "Carro",
+                "quantidade": 7
+            },
+            failOnStatusCode: true,
+            auth: {
+                bearer: Cypress.env("bearer")
+            }
+        })
+    }
+}
